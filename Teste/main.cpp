e8fa1f1bac8bf9992eab3e8023f7b8332d5afc94
andre10dias <andre10dias@ymail.com>
2023-08-31 11:32:53 -0300

    Teste

---
+++ Teste/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int num1, num2, res = 0, i = 0, s, s1 = 1, s2 = 1;
+
+    cout << "Digite o primeiro número: ";
+    cin >> num1;
+
+    cout << "Digite o segundo número: ";
+    cin >> num2;
+
+    if (num1 < 0) {
+        num1 = -num1;
+        s1 = -1;
+    }
+
+    if (num2 < 0) {
+        num2 = -num2;
+        s2 = -1;
+    }
+
+    s = s1 * s2;
+
+    while (i < num2) {
+        res += num1;
+        i++;
+    }
+
+    cout << "Resultado: " << res * s << endl;
+
+    return 0; 
+}
