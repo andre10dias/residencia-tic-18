ff82fa789cda2971a7f0bf25f4af79a8edbc6143
andre10dias <andre10dias@ymail.com>
2023-09-05 10:18:45 -0300

    Atividades aula 9

--- Atividades/Aula 09/Primos/main.cpp
+++ Atividades/Aula 09/Primos/main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main(void) {
     for (int i = 1; i <= 100; i++)
     {
-        int count = 0;
+        int contador = 0;
 
         for (int j = 1; j <= i; j++)
         {
             if (i % j == 0)
             {
-                count++;
+                contador++;
             }
         }
 
-        if (count == 2)
+        if (contador == 2)
         {
             cout << i << endl;
         }
