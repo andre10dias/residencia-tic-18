a263edd718ad71e9557638d83338fd7dad2de1fd
andre10dias <andre10dias@ymail.com>
2023-09-10 00:19:34 -0300

    Media

--- Atividades/Aula 09/Media/main.cpp
+++ Atividades/Aula 09/Media/main.cpp
@@ -1,44 +1,107 @@
 #include <iostream>
-#include<string.h>
+#include <iomanip>
+#include <string.h>
 
 using namespace std;
 
-#define MAX 10
-#define MIN 1
+#define MAX_DIGITOS 10
+#define MAX_ALUNOS 10
+#define MAX_NOTAS 3
 
-int isDigit(char v[MAX]) {
-    for (int i = 0; i < MAX; i++) {
-        cout << "tete: "<< isdigit(v[i]) << endl;
+int isDigit(char v[MAX_ALUNOS]) {
+    int i = 0;
+
+    while (v[i] != '\0') {
         if (!isdigit(v[i])) {
             return 0;
         }
+
+        i++;
     }
 
     return 1;
 }
+
+float calculaMedia(float v[MAX_NOTAS]) {
+    float soma = 0;
+    float media = 0;
+
+    for (int i = 0; i < MAX_NOTAS; i++)
+    {
+        soma += v[i];
+    }
+    
+    media = soma / MAX_NOTAS;
+
+    return media;
+}
  
 int main(void) {
-    char matricula[MAX];
-    char opcao;
+    int colunas = MAX_NOTAS + 1;
+    float alunos[MAX_ALUNOS][colunas];
+    char matriculaTemp[MAX_DIGITOS];
+    string matricula[MAX_DIGITOS];
+    float notas[MAX_NOTAS];
+    char opcao = 83;
+    int qtdAlunos = 0;
+    int qtdDigitos = 0;
 
     do {
-        cout << matricula;
+        system("clear");
         cout << "Informe a matrícula (somente números): ";
-        cin >> matricula;
+        cin >> matriculaTemp;
 
-        cout << isDigit(matricula);
-
-        if (isDigit(matricula) == 0) {
+        if (isDigit(matriculaTemp) == 0) {
             cout << "\nInforme somente números\n\n\n";
         }
         else {
-            cout << endl;
-            cout << "Deseja informar outra matrícula?\n";
-            cout << "(Digite 'S' para sim ou outra para continuar) ";
-            cin >> opcao;
-            system("clear");
+            do {
+                matricula[qtdAlunos] = matriculaTemp;
+
+                for (int j = 0; j < MAX_NOTAS; j++)
+                {
+                    system("clear");
+                    cout << "Matricula do aluno: " << matricula[qtdAlunos] << endl;
+                    cout << "Informe a nota " << j+1 << ": ";
+                    cin >> notas[j];
+
+                    alunos[qtdAlunos][j] = notas[j];
+                    cout << endl;
+                }
+
+                alunos[qtdAlunos][MAX_NOTAS] = calculaMedia(notas); 
+
+                qtdAlunos++;
+                
+                cout << endl;
+                cout << "Deseja informar outra matrícula?\n";
+                cout << "(Digite 'S' para sim ou 'N' para não) ";
+                cin >> opcao;
+                system("clear");
+            } while (int(opcao) != 83 && int(opcao) != 115 && int(opcao) != 78 && int(opcao) != 110);
         }
 
+    } while (int(opcao) == 83 || int(opcao) == 115);
 
-    } while (int(opcao) != 83 && int(opcao) != 115);
+    
+
+    system("clear");
+    cout << "MATRICULA\tNOTA1\t\tNOTA2\t\tNOTA3\t\tMEDIA\n";
+    cout << "=====================================================================\n";
+
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << matricula[i] << "\t";
+
+        for (int j = 0; j < MAX_NOTAS + 1; j++)
+        {
+            cout << fixed << setprecision(1);
+            cout << alunos[i][j] << "\t\t";
+        }
+        
+        cout << endl;
+    }
+
+    cout << endl;
+    return 0;
 }
