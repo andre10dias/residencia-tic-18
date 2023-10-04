f96a7cf6d2320e78f626b01579bdebd275f0603b
andre10dias <andre10dias@ymail.com>
2023-10-04 09:38:09 -0300

    Lançar notas

--- Atividades/Aula 15/avaliaAluno.cpp
+++ Atividades/Aula 15/avaliaAluno.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
+#include <random>
 
 using namespace std;
 
@@ -13,6 +15,7 @@ using namespace std;
 #define PESO_PRATICA 1
 #define PESO_TEORICA 2
 #define MEDIA_APROVACAO 7
+#define MAX_MOCK 12
 
 struct Prova
 {
@@ -35,7 +38,8 @@ void telaCadastrarAluno(vector<Aluno> &listaAlunos);
 bool cadastrarAlunos(vector<Aluno> &listaAlunos, Aluno aluno);
 void cabecalhoLancarNotas(vector<Aluno> &listaAlunos, int index);
 void lancarNotas(vector<Aluno> &listaAlunos);
-void calcularMediaAluno(vector<Aluno> &listaAlunos, int index);
+void calcularMediaAluno_old(vector<Aluno> &listaAlunos, int index);
+void calcularMediaAluno(Aluno aluno);
 float retornarMediaTurma(vector<Aluno> &listaAlunos);
 void telaListarAlunosAprovadosOuReprovados(vector<Aluno> &listaAlunos, bool aprovados, bool reprovados);
 void telaListarAlunosAcimaDaMedia(vector<Aluno> &listaAlunos);
@@ -49,12 +53,14 @@ void systemClear();
 void systemPause();
 void systemPauseAndClear();
 void mockInserirAlunos(vector<Aluno> &listaAlunos, int numeroAlunos);
+// void mockLancarNotas(vector<Aluno> &listaAlunos);
 
 int main(void) 
 {
     vector<Aluno> listaAlunos;
     Aluno aluno;
-    mockInserirAlunos(listaAlunos, 2);
+    mockInserirAlunos(listaAlunos, 5);
+    // mockLancarNotas(listaAlunos);
     menu(listaAlunos);
     cout << endl;
     return 0;
@@ -236,7 +242,7 @@ void lancarNotas(vector<Aluno> &listaAlunos) {
                     cout << "Nota: ";
                     cin >> prova.notaPratica2;
 
-                    calcularMediaAluno(listaAlunos, i);
+                    calcularMediaAluno_old(listaAlunos, i);
                     listaAlunos[i].situacao = listaAlunos[i].media >= MEDIA_APROVACAO ? "APROVADO" : "REPROVADO";
 
                     if (i == tamanho-1)
@@ -256,10 +262,22 @@ void lancarNotas(vector<Aluno> &listaAlunos) {
     } while (opcao != 0);
 }
 
-void calcularMediaAluno(vector<Aluno> &listaAlunos, int index) {
+void calcularMediaAluno_old(vector<Aluno> &listaAlunos, int index) {
     float soma = 0;
 
     Aluno aluno = listaAlunos[index];
+    Prova prova = aluno.prova;
+
+    soma += (prova.notaPratica1 + prova.notaPratica2) * PESO_PRATICA;
+    soma += (prova.notaTeorica1 + prova.notaTeorica2) * PESO_TEORICA;
+
+    aluno.media = soma / 4;
+}
+
+void calcularMediaAluno(Aluno aluno) {
+    float soma = 0;
+
+    // Aluno aluno = listaAlunos[index];
     Prova prova = aluno.prova;
 
     soma += (prova.notaPratica1 + prova.notaPratica2) * PESO_PRATICA;
@@ -283,10 +301,13 @@ float retornarMediaTurma(vector<Aluno> &listaAlunos) {
 void telaListarAlunosAprovadosOuReprovados(vector<Aluno> &listaAlunos, bool aprovados, bool reprovados) {
     string titulo = aprovados ? "aprovados" : reprovados ? "reprovados" : aprovados && reprovados ? "aprovados e reprovados" : "";
 
+    systemClear();
+
     cout << TRACO << endl;
     cout << "\t\t\t\t\tLista de alunos " << titulo << endl;
     cout << TRACO << endl;
     
+    cout << "Matrícula\tNome\t\t\t\tPrática 1\tPrática 2\tTeórica 1\tTeórica 2\tMédia\tSituação" << endl;
     listarAlunos(listaAlunos, aprovados, reprovados, -1);
 }
 
@@ -304,15 +325,26 @@ void listarAlunos(vector<Aluno> &listaAlunos, bool aprovados, bool reprovados, f
     for (Aluno aluno : listaAlunos)
     {   
         Prova prova = aluno.prova;
-        if ((aprovados && aluno.situacao == "APROVADO") || (reprovados && aluno.situacao == "REPROVADO") 
+
+        if ((aprovados && aluno.media >= MEDIA_APROVACAO) || (reprovados && aluno.media < MEDIA_APROVACAO) 
             || (aprovados && reprovados) || (mediaTurma >= 0 && aluno.media > mediaTurma))
         {
-            cout << aluno.matricula << "\t" << aluno.nome << "\t" << prova.notaPratica1 << "\t" << prova.notaPratica2 << "\t"; 
-            cout << prova.notaTeorica1 << "\t" << prova.notaTeorica2 << "\t" << endl;
+            cout << aluno.matricula << "\t\t" << aluno.nome << "\t\t\t";
+            cout << fixed << setprecision(2);
+            cout << prova.notaPratica1 << fixed << setprecision(2) << "\t";
+            cout << fixed << setprecision(2);
+            cout << prova.notaPratica2 << fixed << setprecision(2) << "\t"; 
+            cout << fixed << setprecision(2);
+            cout << prova.notaTeorica1 << fixed << setprecision(2) << "\t";
+            cout << fixed << setprecision(2);
+            cout << prova.notaTeorica2 << fixed << setprecision(2) << "\t";
+            cout << fixed << setprecision(2);
+            cout << aluno.media << fixed << setprecision(2);
+            cout << aluno.situacao << endl;
         }
         else
         {
-            cout << aluno.matricula << "\t" << aluno.nome << endl;
+            cout << aluno.matricula << "\t\t" << aluno.nome << endl;
         }
     }
 }
@@ -389,71 +421,209 @@ void systemPauseAndClear() {
 
 void mockInserirAlunos(vector<Aluno> &listaAlunos, int numeroAlunos) {
     int tam = listaAlunos.size();
-    int max = 12;
     int i = 0;
 
-    while (i < numeroAlunos && i < max)
+    Aluno novoAluno1;
+    novoAluno1.matricula = gerarMatricula(tam);
+    novoAluno1.nome = "Carlos Dias";
+    // novoAluno1.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno1.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno1.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno1.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
     {
-        Aluno novoAluno1;
-        novoAluno1.matricula = gerarMatricula(tam);
-        novoAluno1.nome = "Carlos Dias";
         listaAlunos.push_back(novoAluno1);
-        i++;
-
-        Aluno novoAluno2;
-        novoAluno2.matricula = gerarMatricula(tam);
-        novoAluno2.nome = "Maria Candido";
-        listaAlunos.push_back(novoAluno2);
-        i++;
-
-    //     Aluno novoAluno3;
-    //     novoAluno3.matricula = gerarMatricula(tam);
-    //     novoAluno3.nome = "Fernanda Santos";
-    //     listaAlunos.push_back(novoAluno3);
-
-    //     Aluno novoAluno4;
-    //     novoAluno4.matricula = gerarMatricula(tam);
-    //     novoAluno4.nome = "Francisco Lima";
-    //     listaAlunos.push_back(novoAluno4);
-
-    //     Aluno novoAluno5;
-    //     novoAluno5.matricula = gerarMatricula(tam);
-    //     novoAluno5.nome = "João Brito";
-    //     listaAlunos.push_back(novoAluno5);
-
-    //     Aluno novoAluno6;
-    //     novoAluno6.matricula = gerarMatricula(tam);
-    //     novoAluno6.nome = "Pedro Cardoso";
-    //     listaAlunos.push_back(novoAluno6);
-
-    //     Aluno novoAluno7;
-    //     novoAluno7.matricula = gerarMatricula(tam);
-    //     novoAluno7.nome = "Luana Dias";
-    //     listaAlunos.push_back(novoAluno7);
-
-    //     Aluno novoAluno8;
-    //     novoAluno8.matricula = gerarMatricula(tam);
-    //     novoAluno8.nome = "Agatha Dias";
-    //     listaAlunos.push_back(novoAluno8);
-
-    //     Aluno novoAluno9;
-    //     novoAluno9.matricula = gerarMatricula(tam);
-    //     novoAluno9.nome = "Antônio Ferreira";
-    //     listaAlunos.push_back(novoAluno9);
-
-    //     Aluno novoAluno10;
-    //     novoAluno10.matricula = gerarMatricula(tam);
-    //     novoAluno10.nome = "Bruno Mezenga";
-    //     listaAlunos.push_back(novoAluno10);
-
-    //     Aluno novoAluno11;
-    //     novoAluno11.matricula = gerarMatricula(tam);
-    //     novoAluno11.nome = "Claudia Lopes";
-    //     listaAlunos.push_back(novoAluno11);
-
-    //     Aluno novoAluno12;
-    //     novoAluno12.matricula = gerarMatricula(tam);
-    //     novoAluno12.nome = "Aparecida da Silva";
-    //     listaAlunos.push_back(novoAluno12);
     }
+    i++;
+
+    Aluno novoAluno2;
+    novoAluno2.matricula = gerarMatricula(tam);
+    novoAluno2.nome = "Maria Candido";
+    // novoAluno2.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno2.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno2.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno2.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno2);
+    }
+    i++;
+
+    Aluno novoAluno3;
+    novoAluno3.matricula = gerarMatricula(tam);
+    novoAluno3.nome = "Fernanda Santos";
+    // novoAluno3.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno3.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno3.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno3.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno3);
+    }
+    i++;
+
+    Aluno novoAluno4;
+    novoAluno4.matricula = gerarMatricula(tam);
+    novoAluno4.nome = "Francisco Lima";
+    // novoAluno4.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno4.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno4.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno4.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno4);
+    }
+    i++;
+
+    Aluno novoAluno5;
+    novoAluno5.matricula = gerarMatricula(tam);
+    novoAluno5.nome = "João Brito";
+    // novoAluno5.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno5.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno5.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno5.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno5);
+    }
+    i++;
+
+    Aluno novoAluno6;
+    novoAluno6.matricula = gerarMatricula(tam);
+    novoAluno6.nome = "Pedro Cardoso";
+    // novoAluno6.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno6.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno6.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno6.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno6);
+    }
+    i++;
+
+    Aluno novoAluno7;
+    novoAluno7.matricula = gerarMatricula(tam);
+    novoAluno7.nome = "Luana Dias";
+    // novoAluno7.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno7.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno7.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno7.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno7);
+    }
+    i++;
+
+    Aluno novoAluno8;
+    novoAluno8.matricula = gerarMatricula(tam);
+    novoAluno8.nome = "Agatha Dias";
+    // novoAluno8.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno8.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno8.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno8.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno8);
+    }
+    i++;
+
+    Aluno novoAluno9;
+    novoAluno9.matricula = gerarMatricula(tam);
+    novoAluno9.nome = "Antônio Ferreira";
+    // novoAluno9.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno9.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno9.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno9.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno9);
+    }
+    i++;
+
+    Aluno novoAluno10;
+    novoAluno10.matricula = gerarMatricula(tam);
+    novoAluno10.nome = "Bruno Mezenga";
+    // novoAluno10.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno10.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno10.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno10.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno10);
+    }
+    i++;
+
+    Aluno novoAluno11;
+    novoAluno11.matricula = gerarMatricula(tam);
+    novoAluno11.nome = "Claudia Lopes";
+    // novoAluno11.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno11.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno11.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno11.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno11);
+    }
+    i++;
+
+    Aluno novoAluno12;
+    novoAluno12.matricula = gerarMatricula(tam);
+    novoAluno12.nome = "Aparecida da Silva";
+    // novoAluno12.prova.notaPratica1 = mockLancarNotas(listaAlunos);
+    // novoAluno12.prova.notaPratica2 = mockLancarNotas(listaAlunos);
+    // novoAluno12.prova.notaTeorica1 = mockLancarNotas(listaAlunos);
+    // novoAluno12.prova.notaTeorica2 = mockLancarNotas(listaAlunos);
+    if (i < numeroAlunos && i < MAX_MOCK)
+    {
+        listaAlunos.push_back(novoAluno12);
+    }
+    i++;
 }
+
+// void mockLancarNotas(vector<Aluno> &listaAlunos) {
+//     std::random_device rd;
+//     std::mt19937 gen(rd());
+
+//     std::uniform_int_distribution<int> dist(0, 10);
+//     int numeroRandom = 0;
+
+//     for (Aluno aluno : listaAlunos)
+//     {   
+//         Prova prova = aluno.prova;
+
+//         numeroRandom = dist(gen);
+//         prova.notaPratica1 = numeroRandom;
+
+//         numeroRandom = dist(gen);
+//         prova.notaPratica2 = numeroRandom;
+
+//         numeroRandom = dist(gen);
+//         prova.notaTeorica1 = numeroRandom;
+        
+//         numeroRandom = dist(gen);
+//         prova.notaTeorica2 = numeroRandom;
+
+//         calcularMediaAluno(aluno);
+
+//         // cout << "teste media: " << aluno.media << endl;
+
+//         string situacao = aluno.media >= MEDIA_APROVACAO ? "APROVADO" : "REPROVADO";
+//         aluno.situacao = situacao;
+
+//         // cout << "teste situacao: " << aluno.situacao << endl;
+//         // systemPause();
+//     }
+
+    // for (Aluno aluno : listaAlunos)
+    // {
+    //     cout << "teste notas: " << aluno.prova.notaPratica1 << endl;
+    //     cout << "teste notas: " << aluno.prova.notaPratica2 << endl;
+    //     cout << "teste notas: " << aluno.prova.notaTeorica1 << endl;
+    //     cout << "teste notas: " << aluno.prova.notaTeorica2 << endl;
+
+    //     cout << "teste media: " << aluno.media << endl;
+    //     cout << "teste situacao: " << aluno.situacao << endl;
+    // }
+
+    // systemPause();
+// }
